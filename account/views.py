8c93ff008b9403245f555b88f37988263f8ad2f6
Volkraider <kos49324@gmail.com>
2020-03-20 19:41:51 +0200

    1st commit

---
+++ account/views.py
@@ -0,0 +1,35 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import login, logout
+
+def signup_view(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            #log user in
+            user = form.save()
+            login(request, user)
+            return redirect('articles:list')
+    else:
+        form = UserCreationForm()
+    return render(request, 'account/signup.html', {'form': form})
+
+def login_view(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            #log in user
+            user = form.get_user()
+            login(request, user)
+            if 'next' in request.POST:
+                return redirect(request.POST.get('next'))
+            else:
+                return redirect('articles:list')
+    else:
+        form = AuthenticationForm()
+    return render(request, 'account/login.html', {'form': form})
+
+def logout_view(request):
+    if request.method == 'POST':
+        logout(request)
+        return redirect('articles:list')
